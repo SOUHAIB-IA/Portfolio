32fe8b51a000cefe240a980b57222534f412fe88
SOUHAIB GARAAOUCH <115374122+SOUHAIB-IA@users.noreply.github.com>
2024-04-26 12:03:16 +0100

    Update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import{FaTwitter,FaFacebookF,FaInstagram,FaLinkedinIn, FaGithub} from 'react-icons/fa'
-import imain from "../assets/1 (2).webp"
+import imain from "../assets/1 (2).jpg"
+
+
 function Main() {
   return (
     <div id="main">
@@ -16,13 +18,13 @@ function Main() {
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start  items-center">
           <h1 className="sm:text-5xl text-4xl  text-gray-800 font-serif font-extrabold">I'm Souhaib GARAAOUCH</h1>
           <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
-            I'm a
+            I'm an
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                "full stack devoloper",
+                "AI & Data Science and Data engineering Student",
                 2000, // wait 1s before replacing "Mice" with "Hamsters"
-                "freelancer",
+                " Freelancer | Future Engineer",
                 2000,
               ]}
               wrapper="div"
@@ -32,10 +34,11 @@ function Main() {
             />
           </h2>
           <div className="flex justify-between pt-6 max-w-[200px] w-full">
-            <a href="https://github.com/SOUHAIB-IA" target="_blank"><FaGithub className="cursor-pointer" size={20}/></a>
-            <a href="https://www.facebook.com/souhaib.garaaouch.9" target="_blank"><FaFacebookF className="cursor-pointer" size={20}/></a>
-            <a href="https://www.instagram.com/souhaibgaraaouch/" target="_blank"><FaInstagram className="cursor-pointer" size={20}/></a>
-            <a href="https://www.linkedin.com/in/souhaib-garaaouch-853a011a5/" target="_blank"><FaLinkedinIn className="cursor-pointer" size={20}/></a>
+            <a href="https://github.com/SOUHAIB-IA"><FaGithub className="cursor-pointer" size={20}/></a>
+            <a href="https://www.facebook.com/souhaib.garaaouch.9"><FaFacebookF className="cursor-pointer" size={20}/></a>
+            <a href="https://www.instagram.com/souhaibgaraaouch/"><FaInstagram className="cursor-pointer" size={20}/></a>
+            <a href="www.linkedin.com/in/souhaib-garaaouch-853a011a5"><FaLinkedinIn className="cursor-pointer" size={20}/></a>
+
           </div>
         </div>
       </div>
